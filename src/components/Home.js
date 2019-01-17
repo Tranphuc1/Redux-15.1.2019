42c76dbae22ac4087d02c4d7ef88edac64ada280
unknown <PhucTQ42@wru.vn>
2019-01-17 09:52:16 +0700

    update 17.1.2019

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {FlatList,StyleSheet, View,Text,TouchableOpacity,Image} from 'react-native';
-import { actUpData } from '../actions/actions';
 import { connect } from 'react-redux';
 
 class HomeScreenDetail extends React.Component {
@@ -62,20 +61,21 @@ class HomeScreenDetail extends React.Component {
       }
       );
   }
-  // checkimage = (image) => {
-  //   if(!image) {
-  //     return image ='http://icons.iconarchive.com/icons/treetog/junior/256/folder-blue-pictures-icon.png';
-  //   }else{
-  //     return image;
-  //   }
-  // }
   render() {
     const { navigation } = this.props;
     const image = navigation.getParam('image', 'no-img');
-    // const img = image ? image : 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjHkOXb3PLfAhUS4o8KHShmCFcQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fitunes.apple.com%252Fua%252Fapp%252F50x50%252Fid881859389%253Fmt%253D8%26psig%3DAOvVaw3EYXyuHX3lewBEQAJmkMVs%26ust%3D1547742622453521&psig=AOvVaw3EYXyuHX3lewBEQAJmkMVs&ust=1547742622453521';
-
+    console.log(image);
     return (
       <View style={{flex: 1, paddingTop:20}}> 
+        <TouchableOpacity onPress ={  onPress= () =>{
+            this.props.navigation.navigate('SnapShot');
+          }}>
+          <Text>SNAPSHOT</Text>
+        </TouchableOpacity>
+        <Image
+          style={{width: 150, height: 150}}
+          source={{uri : `${image}`}}
+        />
         <FlatList
         data={this.state.data}
         renderItem={({item,index}) =>
@@ -90,16 +90,6 @@ class HomeScreenDetail extends React.Component {
                 <Text>NAME:{item.task_name}</Text>
                 <Text>Created_at:{item.created_at}</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress ={  onPress= () =>{
-                  this.props.navigation.navigate('SnapShot');
-                }}>
-                <Text>SNAPSHOT</Text>
-              </TouchableOpacity>
-              
-              <Image
-                style={{width: 50, height: 50}}
-                source={{uri : image}}
-              />
             </View>
         }
           keyExtractor={(item) => item.id}
